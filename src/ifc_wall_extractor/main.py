94948d982ed359ba16b2110472c222f49474dff0
Rossi <sebastian.rossi@elkiro.de>
2023-06-07 21:38:03 +0200

    played around with multi meshes and extracted differend kind of ifc classes

--- src/ifc_wall_extractor/main.py
+++ src/ifc_wall_extractor/main.py
@@ -1,40 +1,41 @@
 import ifcopenshell
 from ifcopenshell import geom
-from stl import mesh
+from stl import mesh, Mode
 from OCC import VERSION
 import numpy as np
 
 print("OCC version", VERSION)
 
 # Specify to return pythonOCC shapes from ifcopenshell.geom.create_shape()
+# settings.set(settings.USE_PYTHON_OPENCASCADE, True)
+
 settings = geom.settings()
-#settings.set(settings.USE_PYTHON_OPENCASCADE, True)
+settings.set(settings.USE_WORLD_COORDS, True)
 
 # Open the IFC file using IfcOpenShell
 ifc_file = ifcopenshell.open("../models/sample_house.ifc")
 
 # Display the geometrical contents of the file using Python OpenCascade
-products = ifc_file.by_type("IfcWall")
+products = ifc_file.by_type("IfcProduct")
 print(products)
-vertices = []
-faces = []
-edges = []
+meshes = []
 
-for product in products[-2:-1]:
-    shape = ifcopenshell.geom.create_shape(settings, product)
-    print(shape.geometry.verts, shape.geometry.faces)
-    vertices.extend(shape.geometry.verts)
-    edges.extend(shape.geometry.edges)
-    faces.extend(shape.geometry.faces)
+for product in products:
+    if product.is_a("IfcOpeningElement"):
+        print("moin")
+    if product.Representation and (product.is_a("IfcSite") or product.is_a("IfcStair") or product.is_a("IfcWall")):
+        print(product)
+        shape = ifcopenshell.geom.create_shape(settings, product)
+        vertices = np.array(shape.geometry.verts).reshape((-1, 3))
+        edges = np.array(shape.geometry.edges)
+        faces = np.array(shape.geometry.faces).reshape((-1, 3))
 
-vertices = np.array(vertices).reshape((-1, 3))
-faces = np.array(faces).reshape((-1, 3))
+        m = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+        for i, f in enumerate(faces):
+            for j in range(3):
+                m.vectors[i][j] = vertices[f[j], :]
+        meshes.append(m)
 
-# Create the mesh
-cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
-for i, f in enumerate(faces):
-    for j in range(3):
-        cube.vectors[i][j] = vertices[f[j],:]
-
-# Write the mesh to file "cube.stl"
-cube.save('cube.stl')
+# Create the combined mesh
+combined = mesh.Mesh(np.concatenate([m.data for m in meshes]))
+combined.save('cube.stl', mode=Mode.ASCII)
