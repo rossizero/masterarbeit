c0f0e4b84bfad682c276c1a4a8820cbfb27d2b6b
Rossi <sebastian.rossi@elkiro.de>
2023-07-21 11:25:29 +0200

    wip #12

--- src/code/blender_add_on/ifc_helper.py
+++ src/code/blender_add_on/ifc_helper.py
@@ -8,11 +8,15 @@ import bpy
 import blenderbim.tool as tool
 import time
 import ifcopenshell.util.element
+import numpy as np
 
 
 class IfcModelingHelper:
     last_selection = []
-    reset_settings = []
+    last_object = None
+    grid = None
+    current_transform = 0
+    reset_settings_list = []
     GRID_IDENTIFICATOR = "grid"
 
     @classmethod
@@ -32,48 +36,60 @@ class IfcModelingHelper:
         # selection changed?
         if bpy.context.selected_objects != cls.last_selection:
             cls.last_selection = bpy.context.selected_objects
-            obj = bpy.context.active_object
+            cls.last_object = None
+            cls.grid = None
+            reset = True
+            if len(bpy.context.selected_objects) != 0:
+                obj = bpy.context.selected_objects[0]
 
-            # set global grid to the one that's defined by the active ifcwall
-            if obj.BIMObjectProperties.ifc_definition_id is not None:
-                # save grid settings before changing them so we're able to reset them on selection change
-                cls.save_settings(scene)
+                # set global grid to the one that's defined by the active ifcwall
+                id_ = obj.BIMObjectProperties.ifc_definition_id
+                if id_ is not None and id_ != 0:
+                    cls.ifc_file = tool.Ifc.get()
+                    obj = cls.ifc_file.by_id(obj.BIMObjectProperties.ifc_definition_id)
+                    if obj is not None:
+                        if obj.is_a("IfcWindow") or obj.is_a("IfcDoor"):
+                            # https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcDoor.htm
+                            # does this door/window fill a void
+                            fills = obj.FillsVoids  # [0:1]
+                            if len(fills) > 0:
+                                ifc_rel_fills_element = fills[0]
+                                # which IfcOpeningElements are filled by this IfcRelFillElement
+                                ifc_rel_opening_element = ifc_rel_fills_element.RelatingOpeningElement
+                                void_element = ifc_rel_opening_element.VoidsElements[0]   # [1:1]
+                                building_element = void_element.RelatingBuildingElement
+                                obj = building_element
 
-                cls.ifc_file = tool.Ifc.get()
-                obj = cls.ifc_file.by_id(obj.BIMObjectProperties.ifc_definition_id)
-                if obj is not None:
-                    if obj.is_a("IfcWindow") or obj.is_a("IfcDoor"):
-                        # https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcDoor.htm
-                        # does this door/window fill a void
-                        fills = obj.FillsVoids  # [0:1]
-                        if len(fills) > 0:
-                            ifc_rel_fills_element = fills[0]
-                            # which IfcOpeningElements are filled by this IfcRelFillElement
-                            ifc_rel_opening_element = ifc_rel_fills_element.RelatingOpeningElement
-                            void_element = ifc_rel_opening_element.VoidsElements[0]   # [1:1]
-                            building_element = void_element.RelatingBuildingElement
-                            obj = building_element
-
-                    if obj.is_a("IfcWall"):
-                        obj_type = ifcopenshell.util.element.get_type(obj)
-                        grid = cls.get_grid(obj_type)
-                        if grid:
-                            cls.set_snap_parameters(scene, grid)
-                            print(grid)
-            else:
+                        if obj.is_a("IfcWall"):
+                            obj_type = ifcopenshell.util.element.get_type(obj)
+                            cls.grid = cls.get_grid(obj_type)
+                            # save grid settings before changing them so we're able to reset them on selection change
+                            if cls.grid:
+                                reset = False
+                                cls.save_settings(scene)
+                                cls.set_snap_parameters(scene)
+            if reset:
                 # reset grid settings
                 cls.reset_settings(scene)
-
-
-            print("Selection changed", time.time())
 
         depsgraph = bpy.context.evaluated_depsgraph_get()
 
         for update in depsgraph.updates:
             obj = bpy.context.active_object
             if update.is_updated_transform:
-                print(obj, " is_updated_transform")
-            
+                if cls.last_object is not None:
+                    transform = np.array(cls.last_object.location - obj.location)
+                    ids = np.where(transform != 0.0)
+                    if len(ids) > 0 and len(ids[0]) > 0:
+                        cls.current_transform = int(ids[0][0])
+                        if cls.grid:
+                            for i, val in enumerate(cls.grid):
+                                obj.location[i] = (obj.location[i] / val) * val
+                        print(cls.current_transform)
+                        cls.set_snap_parameters(scene)
+
+                cls.last_object = obj.copy()
+
             if update.is_updated_shading:
                 print(obj, " is_updated_shading")
 
@@ -90,35 +106,38 @@ class IfcModelingHelper:
                         grid_scale = space.overlay.grid_scale
                         break
 
-        cls.reset_settings = [
+        cls.reset_settings_list = [
             grid_scale,
             scene.tool_settings.snap_elements,
             scene.tool_settings.use_snap,
             scene.tool_settings.use_snap_grid_absolute
         ]
+        print("save settings", cls.reset_settings_list)
 
     @classmethod
     def reset_settings(cls, scene):
-        grid_scale = cls.reset_settings[0]
-        for area in bpy.data.screens["Scripting"].areas:
-            if area.type == 'VIEW_3D':
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.overlay.grid_scale = grid_scale if grid_scale is not None else 1
-                        break
+        if len(cls.reset_settings_list) > 0:
+            grid_scale = cls.reset_settings_list[0]
+            for area in bpy.data.screens["Scripting"].areas:
+                if area.type == 'VIEW_3D':
+                    for space in area.spaces:
+                        if space.type == 'VIEW_3D':
+                            space.overlay.grid_scale = grid_scale if grid_scale is not None else 1
+                            break
 
-        scene.tool_settings.snap_elements = cls.reset_settings[1]
-        scene.tool_settings.use_snap = cls.reset_settings[2]
-        scene.tool_settings.use_snap_grid_absolute = cls.reset_settings[3]
+            scene.tool_settings.snap_elements = cls.reset_settings_list[1]
+            scene.tool_settings.use_snap = cls.reset_settings_list[2]
+            scene.tool_settings.use_snap_grid_absolute = cls.reset_settings_list[3]
+            print("reset settings", cls.reset_settings_list)
 
 
     @classmethod
-    def set_snap_parameters(cls, scene, grid: []):
+    def set_snap_parameters(cls, scene):
         for area in bpy.data.screens["Scripting"].areas:
             if area.type == 'VIEW_3D':
                 for space in area.spaces:
                     if space.type == 'VIEW_3D':
-                        space.overlay.grid_scale = grid[0]
+                        space.overlay.grid_scale = cls.grid[cls.current_transform]
                         break
 
         scene.tool_settings.use_snap = True
@@ -129,7 +148,7 @@ class IfcModelingHelper:
         # https://docs.blender.org/api/current/bpy.types.DepsgraphUpdate.html
 
 def register():
-    # bpy.app.handlers.depsgraph_update_post.clear()
+    bpy.app.handlers.depsgraph_update_post.clear()
     bpy.app.handlers.depsgraph_update_post.append(IfcModelingHelper.on_depsgraph_update)
 
 def unregister():
