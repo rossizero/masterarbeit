a16a9daa36b5304733c99bf5a79dc0d39259f86e
Rossi <sebastian.rossi@elkiro.de>
2023-07-28 15:08:17 +0200

    ifc_helper kinda works now

--- src/code/blender_add_on/ifc_helper.py
+++ src/code/blender_add_on/ifc_helper.py
@@ -6,21 +6,45 @@ bl_info = {
 
 import bpy
 import blenderbim.tool as tool
-import time
 import ifcopenshell.util.element
-import numpy as np
 
 
 class IfcModelingHelper:
     last_selection = []
-    last_object = None
     grid = None
-    current_transform = 0
-    reset_settings_list = []
     GRID_IDENTIFICATOR = "grid"
 
     @classmethod
-    def get_grid(cls, ifc_type):
+    def look_for_grid(cls, obj):
+        # set global grid to the one that's defined by the active ifcwall
+        grid = None
+        id_ = obj.BIMObjectProperties.ifc_definition_id
+        if id_ is not None and id_ != 0:
+            cls.ifc_file = tool.Ifc.get()
+            obj = cls.ifc_file.by_id(obj.BIMObjectProperties.ifc_definition_id)
+            if obj is not None:
+                if obj.is_a("IfcWindow") or obj.is_a("IfcDoor"):
+                    # https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcDoor.htm
+                    # does this door/window fill a void
+                    fills = obj.FillsVoids  # [0:1]
+                    if len(fills) > 0:
+                        ifc_rel_fills_element = fills[0]
+                        # which IfcOpeningElements are filled by this IfcRelFillElement
+                        ifc_rel_opening_element = ifc_rel_fills_element.RelatingOpeningElement
+                        void_element = ifc_rel_opening_element.VoidsElements[0]  # [1:1]
+                        building_element = void_element.RelatingBuildingElement
+                        obj = building_element
+                if obj.is_a("IfcOpeningElement"):
+                    void_element = obj.VoidsElements[0]  # [1:1]
+                    building_element = void_element.RelatingBuildingElement
+                    obj = building_element
+                if obj.is_a("IfcWall"):
+                    obj_type = ifcopenshell.util.element.get_type(obj)
+                    grid = cls.get_grid_from_type(obj_type)
+        return grid
+
+    @classmethod
+    def get_grid_from_type(cls, ifc_type):
         type_psets = ifcopenshell.util.element.get_psets(ifc_type)
         if type_psets is not None:
             for name in type_psets.keys():
@@ -30,146 +54,45 @@ class IfcModelingHelper:
                     return g
         return None
 
+    @classmethod
+    def apply_grid(cls, obj, grid):
+        if grid:
+            for i, val in enumerate(grid):
+                obj.location[i] = round((obj.location[i] / val)) * val
+            obj.dimensions = [round((obj.dimensions[i] / val)) * val for i, val in enumerate(grid)]
 
     @classmethod
     def on_depsgraph_update(cls, scene, _):
-        # selection changed?
         if bpy.context.selected_objects != cls.last_selection:
             cls.last_selection = bpy.context.selected_objects
-            cls.last_object = None
-            cls.grid = None
-            reset = True
+
             if len(bpy.context.selected_objects) != 0:
                 obj = bpy.context.selected_objects[0]
+                cls.grid = cls.look_for_grid(obj)
 
-                # set global grid to the one that's defined by the active ifcwall
-                id_ = obj.BIMObjectProperties.ifc_definition_id
-                if id_ is not None and id_ != 0:
-                    cls.ifc_file = tool.Ifc.get()
-                    obj = cls.ifc_file.by_id(obj.BIMObjectProperties.ifc_definition_id)
-                    if obj is not None:
-                        if obj.is_a("IfcWindow") or obj.is_a("IfcDoor"):
-                            # https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcDoor.htm
-                            # does this door/window fill a void
-                            fills = obj.FillsVoids  # [0:1]
-                            if len(fills) > 0:
-                                ifc_rel_fills_element = fills[0]
-                                # which IfcOpeningElements are filled by this IfcRelFillElement
-                                ifc_rel_opening_element = ifc_rel_fills_element.RelatingOpeningElement
-                                void_element = ifc_rel_opening_element.VoidsElements[0]   # [1:1]
-                                building_element = void_element.RelatingBuildingElement
-                                obj = building_element
-
-                        if obj.is_a("IfcWall"):
-                            obj_type = ifcopenshell.util.element.get_type(obj)
-                            cls.grid = cls.get_grid(obj_type)
-                            # save grid settings before changing them so we're able to reset them on selection change
-                            if cls.grid:
-                                reset = False
-                                cls.save_settings(scene)
-                                cls.set_snap_parameters(scene)
-            if reset:
-                # reset grid settings
-                cls.reset_settings(scene)
+                cls.apply_grid(bpy.context.selected_objects[0], cls.grid)
 
         depsgraph = bpy.context.evaluated_depsgraph_get()
 
         for update in depsgraph.updates:
             obj = bpy.context.active_object
-            if update.is_updated_transform:
-                if cls.last_object is not None:
-                    transform = np.array(cls.last_object.location - obj.location)
-                    ids = np.where(transform != 0.0)
-                    if len(ids) > 0 and len(ids[0]) > 0:
-                        old_ = cls.current_transform
-                        cls.current_transform = int(ids[0][0])
-                        if old_ != cls.current_transform:
-                            print(cls.current_transform)
-                            cls.set_snap_parameters(scene)
+            # cls.grid = cls.look_for_grid(obj)
 
-                        if cls.grid:
-                            for i, val in enumerate(cls.grid):
-                                obj.location[i] = (obj.location[i] / val) * val
-
-                cls.last_object = obj.copy()
-
+            if update.is_updated_transform or update.is_updated_geometry:
+                cls.apply_grid(obj, cls.grid)
             if update.is_updated_shading:
-                print(obj, " is_updated_shading")
+                pass
 
-            if update.is_updated_geometry:
-                print(obj, " is_updated_geometry")
-
-    @classmethod
-    def save_settings(cls, scene):
-        grid_scale = None
-        for area in bpy.data.screens["Scripting"].areas:
-            if area.type == 'VIEW_3D':
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        grid_scale = space.overlay.grid_scale
-                        break
-
-        cls.reset_settings_list = [
-            grid_scale,
-            scene.tool_settings.snap_elements,
-            scene.tool_settings.use_snap,
-            scene.tool_settings.use_snap_grid_absolute
-        ]
-        print("save settings", cls.reset_settings_list)
-
-    @classmethod
-    def reset_settings(cls, scene):
-        if len(cls.reset_settings_list) > 0:
-            grid_scale = cls.reset_settings_list[0]
-            for area in bpy.data.screens["Scripting"].areas:
-                if area.type == 'VIEW_3D':
-                    for space in area.spaces:
-                        if space.type == 'VIEW_3D':
-                            space.overlay.grid_scale = grid_scale if grid_scale is not None else 1
-                            break
-
-            scene.tool_settings.snap_elements = cls.reset_settings_list[1]
-            scene.tool_settings.use_snap = cls.reset_settings_list[2]
-            scene.tool_settings.use_snap_grid_absolute = cls.reset_settings_list[3]
-            print("reset settings", cls.reset_settings_list)
-
-    @classmethod
-    def set_snap_parameters(cls, scene):
-        for area in bpy.context.screen.areas:
-            if area.type == 'VIEW_3D':
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.overlay.grid_scale = cls.grid[cls.current_transform]
-
-                        # reset tool
-                        mode = bpy.context.mode
-                        print(mode)
-                        current_tool = bpy.context.workspace.tools.from_space_view3d_mode(mode, create=False).idname
-                        print(current_tool)
-                        bpy.ops.wm.tool_set_by_id(name=current_tool)
-
-                        override = bpy.context.copy()
-                        override["space_data"] = space
-                        override["area"] = area
-                        bpy.ops.wm.tool_set_by_id(override, name=current_tool)
-                        break
-
-        scene.tool_settings.use_snap = True
-        scene.tool_settings.use_snap_grid_absolute = True
-        scene.tool_settings.snap_elements = {'INCREMENT'}
-        # https://blender.stackexchange.com/questions/154610/how-do-you-programatically-set-grid-scale
-        # https://prosperocoder.com/posts/blender/blender-python-transformations/
-        # https://docs.blender.org/api/current/bpy.types.DepsgraphUpdate.html
 
 def register():
     bpy.app.handlers.depsgraph_update_post.clear()
     bpy.app.handlers.depsgraph_update_post.append(IfcModelingHelper.on_depsgraph_update)
 
+
 def unregister():
     bpy.app.handlers.depsgraph_update_post.remove(IfcModelingHelper.on_depsgraph_update)
 
 
-# This allows you to run the script directly from Blender's Text editor
-# to test the add-on without having to install it.
+# This allows to run the script directly from Blender's Text editor
 if __name__ == "__main__":
     register()
