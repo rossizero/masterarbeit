a036156cf089321dfc36b71bf8d65ec07e983e1c
Rossi <sebastian.rossi@elkiro.de>
2023-08-07 15:43:27 +0200

    ffff

--- src/code/wall_detailing/wall_detailer.py
+++ src/code/wall_detailing/wall_detailer.py
@@ -20,8 +20,8 @@ from OCC.Core.gp import gp_Pnt, gp_Quaternion, gp_Trsf, gp_Ax3, gp_Vec
 
 class BrickInformation:
     def __init__(self, length, width, height):
-        self.length = length
-        self.width = width
+        self.length = max(length, width)
+        self.width = min(length, width)
         self.height = height
 
     def volume(self):
@@ -35,19 +35,20 @@ class Brick(BrickInformation):
         self.rotation = rotation
 
     def get_brep_shape(self):
-        corner = gp_Pnt(self.position[0], self.position[1], self.position[2])
+        offset = 0.01
+        corner = gp_Pnt(0, 0, 0)
 
-        # numpy to occ quaternion
-        vector_part = np.array([self.rotation.x, self.rotation.y, self.rotation.z])
-        rotation = gp_Quaternion(self.rotation.w, *vector_part)
-        # rotate
-        transformation = gp_Trsf()
-        transformation.SetRotation(rotation)
-        shape = BRepPrimAPI_MakeBox(corner, self.width, self.length, self.height).Shape()
-        shape = BRepBuilderAPI_Transform(shape, transformation).Shape()
+        shape = BRepPrimAPI_MakeBox(corner, self.length - offset, self.width - offset, self.height - offset).Shape()
+
         # translate
         transformation = gp_Trsf()
         transformation.SetTranslation(gp_Vec(*self.position))
+        shape = BRepBuilderAPI_Transform(shape, transformation).Shape()
+
+        # rotate
+        transformation = gp_Trsf()
+        rotation = gp_Quaternion(self.rotation.x, self.rotation.y, self.rotation.z, self.rotation.w)
+        transformation.SetRotation(rotation)
         shape = BRepBuilderAPI_Transform(shape, transformation).Shape()
         return shape
 
@@ -85,6 +86,7 @@ class WallDetailer:
 
         # Get the minimum and maximum coordinates of the bounding box
         xmin, ymin, zmin, xmax, ymax, zmax = bounding_box.Get()
+        min_max_array_bbox = np.around(np.array(bounding_box.Get()), decimals=6)
 
         # Calculate the dimensions of the rotated box
         length = xmax - xmin
@@ -107,35 +109,32 @@ class WallDetailer:
 
         gprops = GProp_GProps()
         brepgprop_VolumeProperties(shape, gprops)
+
         # check if boundingbox has the same vertices as our axis aligned shape
         # and if their volumes are equal (if not there are openings inside the shape)
-        print(np.array([min(coords[0]), min(coords[1]), min(coords[2]),
-                                                 max(coords[0]), max(coords[1]), max(coords[2])]),
-                                        np.array([xmin, ymin, zmin, xmax, ymax, zmax]))
-        print(np.isclose(np.array([min(coords[0]), min(coords[1]), min(coords[2]),
-                                                 max(coords[0]), max(coords[1]), max(coords[2])]),
-                                        np.array([xmin, ymin, zmin, xmax, ymax, zmax])))
-        return (False not in np.isclose(np.array([min(coords[0]), min(coords[1]), min(coords[2]),
-                                                 max(coords[0]), max(coords[1]), max(coords[2])]),
-                                        np.array([xmin, ymin, zmin, xmax, ymax, zmax])) and
-                np.isclose(gprops.Mass(), length * width * height))
+        min_max_array_wall = np.around(np.array([min(coords[0]), min(coords[1]), min(coords[2]),
+                                       max(coords[0]), max(coords[1]), max(coords[2])]), decimals=6)
+        close = np.allclose(min_max_array_wall, min_max_array_bbox) or np.allclose(min_max_array_bbox, min_max_array_wall)
+        return close and np.isclose(gprops.Mass(), length * width * height)
 
     def detail_wall(self, wall: Wall, bricks: List[BrickInformation]):
         print(wall, bricks)
         brick_ret = []
-        print(self.is_cubic(wall.occ_shape))
         if self.is_cubic(wall.occ_shape):
             # Apply the inverted translation and rotation to our shape to get axis aligned shape at [0, 0, 0]
             transformation = gp_Trsf()
+            original_translation = np.array([wall.occ_shape.Location().Transformation().TranslationPart().X(),
+                                             wall.occ_shape.Location().Transformation().TranslationPart().Y(),
+                                             wall.occ_shape.Location().Transformation().TranslationPart().Z()])
             transformation.SetTranslation(gp_Vec(wall.occ_shape.Location().Transformation().TranslationPart().Reversed()))
             shape = BRepBuilderAPI_Transform(wall.occ_shape, transformation).Shape()
 
             shape = shape.Reversed()
             transformation = gp_Trsf()
-            original_rotation = np.quaternion(shape.Location().Transformation().GetRotation().X(),
+            original_rotation = np.quaternion(shape.Location().Transformation().GetRotation().W(),
+                                              shape.Location().Transformation().GetRotation().X(),
                                               shape.Location().Transformation().GetRotation().Y(),
-                                              shape.Location().Transformation().GetRotation().Z(),
-                                              shape.Location().Transformation().GetRotation().W())
+                                              shape.Location().Transformation().GetRotation().Z())
             transformation.SetRotation(shape.Location().Transformation().GetRotation().Inverted())
             shape = BRepBuilderAPI_Transform(shape, transformation).Shape()
 
@@ -147,7 +146,7 @@ class WallDetailer:
                   shape.Location().Transformation().GetRotation().Y(),
                   shape.Location().Transformation().GetRotation().Z(),
                   shape.Location().Transformation().GetRotation().W())
-
+            print("og", original_rotation)
             # create a boundingbox around the shape
             bounding_box = Bnd_Box()
             brepbndlib_Add(shape, bounding_box)
@@ -159,6 +158,9 @@ class WallDetailer:
             length = round(xmax - xmin, 6)
             width = round(ymax - ymin, 6)
             height = round(zmax - zmin, 6)
+            tmp = length
+            length = max(length, width)
+            width = min(tmp, width)
             print(length, width, height)
 
             # get "biggest" brick TODO maybe there is a better criteria?
@@ -173,17 +175,17 @@ class WallDetailer:
             length_step = module.width if rotate else module.length
             length_steps = int(length / length_step)
             length_leftover = length % length_step
-            print(rotate, length_steps, length_leftover)  # TODO what if leftover
+            print(rotate, length_step, length_steps, length_leftover)  # TODO what if leftover
 
+            r = math.pi / 2.0 if not rotate else 0
             for j in range(num_layers):
                 offset = j % 2 * length_step / 2.0  # TODO
-                #for i in range(length_steps - j % 2):
-                pos = np.array([offset + 0 * length_step, 0, j * module.height])
-                r = math.pi / 2.0 if rotate else 0
-                rot = np.quaternion(0, 0, 0, 1) #quaternion.from_euler_angles(0, 0, r)
-                print(pos, r, rot)
-                b = Brick(module.length, module.width, module.height, pos, rot)
-                brick_ret.append(b)
+                for i in range(length_steps - j % 2):
+                    pos = np.array([offset + i * length_step, 0, j * module.height]) + original_translation
+                    rot = original_rotation
+                    print(pos, r, rot)
+                    b = Brick(module.length, module.width, module.height, pos, rot)
+                    brick_ret.append(b)
 
             #for b in brick_ret:
             #    b.position += np.array([xmin, ymin, zmin])
@@ -208,7 +210,7 @@ class WallDetailer:
         return bricks
 
     @classmethod
-    def convert_to_stl(cls, bricks: [Brick], path: str, detail: float = 0.1):
+    def convert_to_stl(cls, bricks: [Brick], path: str, detail: float = 0.1, additional_shapes: List = None):
         import os
         file_path = os.path.abspath(path)
         bricks_copy = bricks.copy()
@@ -222,6 +224,14 @@ class WallDetailer:
                     shape,
                     brick.get_brep_shape()
                 ).Shape()
+
+            if additional_shapes is not None:
+                for s in additional_shapes:
+                    shape = BRepAlgoAPI_Fuse(
+                        shape,
+                        s
+                    ).Shape()
+
             mesh = BRepMesh_IncrementalMesh(shape, detail)
             mesh.Perform()
             assert mesh.IsDone()
@@ -230,18 +240,20 @@ class WallDetailer:
 
 
 def make_occ_box(length, width, height, position, rotation):
-    corner = gp_Pnt(position[0], position[1], position[2])
+    corner = gp_Pnt(0, 0, 0)
 
-    # numpy to occ quaternion
-    vector_part = np.array([rotation.x, rotation.y, rotation.z])
-    rotation = gp_Quaternion(rotation.w, *vector_part)
-    transformation = gp_Trsf()
-    transformation.SetRotation(rotation)
     shape = BRepPrimAPI_MakeBox(corner, length, width, height).Shape()
-    shape = BRepBuilderAPI_Transform(shape, transformation).Shape()
+
     transformation = gp_Trsf()
     transformation.SetTranslation(gp_Vec(*position))
     shape = BRepBuilderAPI_Transform(shape, transformation).Shape()
+
+    # numpy to occ quaternion
+    rotation = gp_Quaternion(rotation.x, rotation.y, rotation.z, rotation.w)
+    transformation = gp_Trsf()
+    transformation.SetRotation(rotation)
+    shape = BRepBuilderAPI_Transform(shape, transformation).Shape()
+
     gprops = GProp_GProps()
 
     # Check if the shape is a solid
@@ -251,11 +263,11 @@ def make_occ_box(length, width, height, position, rotation):
 
 
 if __name__ == "__main__":
-    wall = Wall(make_occ_box(10, 1, 5, [1, 1, 1], quaternion.from_euler_angles(0.3, 1, math.pi/3.0)))
+    wall = Wall(make_occ_box(10, 1, 5, [1, 1, 1], quaternion.from_euler_angles(0, 0, math.pi/3)))
     wall.ifc_wall_type = "test"
     brick_information = {"test": [BrickInformation(2, 1, 0.5), BrickInformation(1, 0.5, 0.5)]}
     walls = [wall]
     wall_detailer = WallDetailer(walls, brick_information)
     bb = wall_detailer.detail()
     #b = [Brick(1, 2, 0.5, np.ones(shape=(3,)) * i, quaternion.from_euler_angles(math.pi * 2 / 20.0 * i, 0, 0)) for i in range(80)]
-    WallDetailer.convert_to_stl(bb, "output.stl")
+    WallDetailer.convert_to_stl(bb, "output.stl", additional_shapes=[wall.occ_shape])
