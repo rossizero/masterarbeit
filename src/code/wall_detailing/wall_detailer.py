ad63bba16171900d0a251a653090e75c6b31438c
Rossi <sebastian.rossi@elkiro.de>
2023-08-20 15:02:47 +0200

    eee

--- src/code/wall_detailing/wall_detailer.py
+++ src/code/wall_detailing/wall_detailer.py
@@ -90,7 +90,7 @@ class WallDetailer:
 
         # translate mid of new wall to 0 0 0
         transformation = gp_Trsf()
-        complete_translation = translation - (np.array([wall.length, wall.width, wall.height]) - scale) / 2.0
+        complete_translation = translation + (np.array([wall.length, wall.width, wall.height] - scale) / 2.0)
         translation = gp_Vec(*complete_translation).Reversed()
         transformation.SetTranslation(translation)
 
@@ -241,7 +241,7 @@ if __name__ == "__main__":
     w2 = make_wall(10, 1, 5, np.array([-21.0, 0.0, 0.0]), quaternion.from_euler_angles(0, 1.3, math.pi / 2), ifc_wall_type="test")
     w3 = make_wall(20, 1, 5, np.array([-16.0, 5.0, 0.0]), quaternion.from_euler_angles(0, 1.3, math.pi / 2), ifc_wall_type="test")
     w4 = make_wall(10, 1, 5, np.array([4.5, -5.5, 0.0]), quaternion.from_euler_angles(0, 0, 2 * math.pi), ifc_wall_type="test")
-    walls = [w1, w2, w3, w4]
+    walls = [w1, w2]#, w3, w4]
 
     wallss = walls.copy()
     wall_detailer = WallDetailer(wallss, brick_information)
@@ -249,4 +249,4 @@ if __name__ == "__main__":
 
     print("walls", len(wallss))
     WallDetailer.convert_to_stl([], "base.stl", additional_shapes=[w.get_shape() for w in walls])
-    WallDetailer.convert_to_stl(bb[:1], "output.stl", additional_shapes=[w.get_shape() for w in wallss])
+    WallDetailer.convert_to_stl(bb, "output.stl", additional_shapes=[w.get_shape() for w in wallss])
