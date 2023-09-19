c4598848f6876cb51f9c3b67099fd4598cd2cfed
Rossi <sebastian.rossi@elkiro.de>
2023-09-19 13:28:41 +0200

    mini wip layering

--- src/code/wall_detailing/detailing/wall_layer.py
+++ src/code/wall_detailing/detailing/wall_layer.py
@@ -1,12 +1,18 @@
+import numpy as np
+
+from wall_detailing.masonry.brick import BrickInformation
 
 
 class WallLayer:
     """
     A straight layer of a wall
     """
-    def __init__(self):
-        pass
+    def __init__(self, module: BrickInformation, length: float, translation: np.array = np.array([0.0, 0.0, 0.0]), height: float = None):
+        self.module = module
+        self.translation = translation  # relative to walls translation
+        self.height = module.height if height is None else height
+        self.length = length
 
-    def combine(self, other: WallLayer):
+    def combine(self, other: 'WallLayer'):
         ret = WallLayer()
         return ret
--- src/code/wall_detailing/detailing/wall_layer_group.py
+++ src/code/wall_detailing/detailing/wall_layer_group.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 from detailing.wall import Wall
+from detailing.wall_layer import WallLayer
 from masonry.brick import BrickInformation
 
 
@@ -6,9 +9,28 @@ class WallLayerGroup:
     def __init__(self, module: BrickInformation):
         self.module = module
         self.layers = []
-
+        self.rotation = np.quaternion(1, 0, 0, 0)
+        self.translation = np.array([0, 0, 0])  # of wall mid
 
     @classmethod
     def from_wall(cls, wall: Wall, module: BrickInformation):
         ret = cls(module)
+        ret.rotation = wall.get_rotation()
+        ret.translation = wall.get_translation()
+        length, width, height = wall.width, wall.width, wall.height
+
+        layers = int(height / module.height)
+        leftover = height % module.height
+        z = -height * 0.5
+        for layer in range(layers):
+            translation = ret.translation.copy()
+            translation[2] = z + (layer + 1) * module.height * 0.5  # z position relative to the walls center
+            wall_layer = WallLayer(module, length, translation=translation)
+            ret.layers.append(wall_layer)
+
+        if leftover > 0.0:
+            translation = ret.translation.copy()
+            translation[2] = height * 0.5 - leftover * 0.5  # TODO: Check if this is correct
+            wall_layer = WallLayer(module, length, translation=translation, height=leftover)
+            ret.layers.append(wall_layer)
         return ret
