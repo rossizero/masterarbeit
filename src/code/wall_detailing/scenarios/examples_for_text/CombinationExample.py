288457911839745dbf4334af1b89ea9e179435da
Sebastian Rossi <sebastian.rossi@elkiro.de>
2023-11-12 17:17:06 +0100

    Added another Wandstück to Real Combination

--- src/code/wall_detailing/scenarios/examples_for_text/CombinationExample.py
+++ src/code/wall_detailing/scenarios/examples_for_text/CombinationExample.py
@@ -11,26 +11,31 @@ class CombinationExampleForText(Scenario):
     def get_walls(self):
         width = 1
         height = 5
-        w0 = Wall.make_wall(9, width, height * 0.5, np.array([-3.5, 1.0, height / 4.0]),
+        w1 = Wall.make_wall(9, width, height * 0.5, np.array([-3.5, 1.0, height / 4.0]),
                             quaternion.from_euler_angles(0, 0, 0 * math.pi),
                             ifc_wall_type="test", name="w0")
 
-        w1 = Wall.make_wall(9, width, height * 0.5, np.array([-3.5 + 4, 1.0, height / 4.0 + 1.5]),
+        w2 = Wall.make_wall(9, width, height * 0.5, np.array([-3.5 + 4, 1.0, height / 4.0 + 1.5]),
                             quaternion.from_euler_angles(0, 0, 0 * math.pi),
                             ifc_wall_type="test", name="w1")
 
-        w2 = Wall.make_wall(3, width, 3 * 0.5, np.array([-3.5 + 4 + 6, 1.0, height / 4.0 + 1.5]),
+        w3 = Wall.make_wall(3, width, 3 * 0.5, np.array([-3.5 + 4 + 6, 1.0, height / 4.0 + 1.5]),
                             quaternion.from_euler_angles(0, 0, 0 * math.pi),
                             ifc_wall_type="test", name="w2")
 
-        w3 = Wall.make_wall(3, width, 3 * 0.5, np.array([-3.5 + 4 + 6, 1.0, height / 4.0 - 0.5]),
+        w4 = Wall.make_wall(3, width, 3 * 0.5, np.array([-3.5 + 4 + 6, 1.0, height / 4.0 - 0.5]),
                              quaternion.from_euler_angles(0, 0, 0 * math.pi),
                              ifc_wall_type="test", name="w3")
 
-        w4 = Wall.make_wall(7, width, height * 0.5, np.array([-3.5, 1.0, height / 4.0 + 4.0]),
+        w5 = Wall.make_wall(7, width, height * 0.5, np.array([-3.5, 1.0, height / 4.0 + 4.0]),
                             quaternion.from_euler_angles(0, 0, 0 * math.pi),
                             ifc_wall_type="test", name="w4")
-        w4_Same = Wall.make_wall(7, width, height * 0.5, np.array([-3.5, 1.0, height / 4.0 + 4.0]),
+
+        w6 = Wall.make_wall(7, width, height * 0.5, np.array([-3.5, 1.0, height / 4.0 + 4.0]),
                                  quaternion.from_euler_angles(0, 0, 0 * math.pi),
                                  ifc_wall_type="test", name="w4_same")
-        return [w0, w1, w2, w3, w4, w4_Same]
+
+        w7 = Wall.make_wall(4, width, 3 * 0.5, np.array([-3.5 + 6.5, 1.0, height / 4.0 + 3.5]),
+                            quaternion.from_euler_angles(0, 0, 0 * math.pi),
+                            ifc_wall_type="test", name="w3")
+        return [w1, w2, w3, w4, w5, w6, w7]
