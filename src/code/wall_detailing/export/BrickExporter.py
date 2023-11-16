34c9f7d55d8add9aa45f46aba29cdcc26991d771
Rossi <sebastian.rossi@elkiro.de>
2023-11-16 14:14:44 +0100

    .....

--- src/code/wall_detailing/export/BrickExporter.py
+++ src/code/wall_detailing/export/BrickExporter.py
@@ -24,9 +24,6 @@ class BrickExportInformation:
         for b in brick.neighbors[Neighbor.BOTTOM]:
             self.depends_on.append(b.id)
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-
 
 class BrickExporter:
     def __init__(self, bricks: List[Brick]):
@@ -44,7 +41,7 @@ class BrickExporter:
         """
         ret = []
         for b in self.bricks:
-            ret.append(BrickExportInformation(b).toJSON())
+            ret.append(BrickExportInformation(b))
 
         with open(path, 'w') as outfile:
-            json.dump(ret, outfile, indent=4)
+            json.dump(ret, outfile, default=lambda o: o.__dict__, sort_keys=True, indent=4)
