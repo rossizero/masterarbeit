fixed center calculation for bricks

src/code/wall_detailing/masonry/brick.py
@@ -166,8 +166,8 @@ class Brick:
         """
         :return: center of the brick in global coordinates
         """
-        mid = self._bottom_left_corner_offset()
-        return self.position - mid
+        mid = quaternion.rotate_vectors(self.orientation, np.array([self.length / 2.0, self.width / 2.0, self.height / 2.0]))
+        return self.position + mid
 
     def _bottom_left_corner_offset(self):
         """

src/code/wall_detailing/wall_detailer.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
     brick_information = {"test": [BrickInformation(2, 1, 0.5, grid=np.array([1, 1, 0.5])),
                                   BrickInformation(1, 1, 0.5, grid=np.array([1, 1, 0.5]))]}
     scenario = CombinationExampleForText()
-    #scenario = DoppelEck2_Closed_TJoint()
+    scenario = DoppelEck2_Closed_TJoint()
     #scenario = SimpleCorner()
 
     WallDetailer.convert_to_stl([], "base.stl", additional_shapes=[w.get_shape() for w in scenario.walls])