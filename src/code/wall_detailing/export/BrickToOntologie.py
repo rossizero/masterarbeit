8bb410ecbae4a9c43c11dd5c665fe54d7d1a1391
Sebastian Rossi <sebastian.rossi@elkiro.de>
2023-11-25 22:11:59 +0100

    more ontology trying

--- src/code/wall_detailing/export/BrickToOntologie.py
+++ src/code/wall_detailing/export/BrickToOntologie.py
@@ -3,28 +3,39 @@ from owlready2 import *
 
 from wall_detailing.masonry.brick import Brick, Neighbor
 
-
+# TODO this returns nothing: Brick and (dependsOn min 0 PlacedBrick) and not(dependsOn some PlaceableBrick)
+# TODO                       Brick and dependsOn some PlacedBrick and not(dependsOn some PlaceableBrick)
 class BrickToOntology:
     def __init__(self, bricks: List[Brick]):
+        self.original_file = "file:///home/rosrunner/Desktop/repos/masterarbeit/src/ontologies/brick_deduction.rdf"
+        self.working_file = "/home/rosrunner/Desktop/repos/masterarbeit/src/ontologies/temporary_working_env.rdf"
+
+        if os.name == 'nt':  # windows
+            owlready2.JAVA_EXE = "C:\\Program Files\\Java\\jre-1.8\\bin\\java.exe"
+            self.original_file = "E:\\Git\\masterarbeit\\src\\ontologies\\brick_deduction.rdf"
+            self.working_file = "E:\\Git\\masterarbeit\\src\\ontologies\\temporary_working_env.rdf"
+
         self.bricks = bricks
-        self.onto = get_ontology("file:///home/rosrunner/Desktop/repos/masterarbeit/src/ontologies/brick_deduction.rdf").load()
+        self.onto = get_ontology(self.original_file).load()
         print(list(self.onto.classes()))
 
         # set ids
         idd = 1
         dic = {}
         for b in bricks:
-            brick = self.onto.Brick("brick_" + str(idd))
+            brick = self.onto.ConcreteBrick("brick_" + str(idd))
+            #brick.is_a.append(self.onto.ConcreteBrick)
             brick.hasID = [idd]
-            brick.dependsOn = []
-            # brick.hasBeenSet = [False]
-
+            brick.hasBeenSet = False
             b.id = idd
             idd += 1
             dic[b.id] = (b, brick)
 
         building = self.onto.Building("building_1")
-        for b in self.onto.Brick.instances():
+        empty = self.onto.PlacedBrick("empty_brick")
+        for b in self.onto.ConcreteBrick.instances():
+            if b == empty:
+                continue
             local_brick = dic[b.hasID[0]][0]
             b.hasBottomNeighbor = [dic[obj.id][1] for obj in local_brick.neighbors[Neighbor.BOTTOM]]
             b.hasTopNeighbor = [dic[obj.id][1] for obj in local_brick.neighbors[Neighbor.TOP]]
@@ -32,15 +43,20 @@ class BrickToOntology:
             b.hasRightNeighbor = [dic[obj.id][1] for obj in local_brick.neighbors[Neighbor.RIGHT]]
             b.hasFrontNeighbor = [dic[obj.id][1] for obj in local_brick.neighbors[Neighbor.FRONT]]
             b.hasBackNeighbor = [dic[obj.id][1] for obj in local_brick.neighbors[Neighbor.BACK]]
+            b.hasBeenSet = False
+            b.dependsOn = [empty]#, dic[1][1]]
             building.hasBrick.append(b)
 
         with self.onto:
-            rule = Imp(1)
+            pass
+            #rule = Imp(1)
             #rule.set_as_rule("""Brick(?b), hasBottomNeighbor(?b, ?n) -> dependsOn(?b, ?n)""")
-            rule.set_as_rule("""Brick(?b) -> hasBeenSet(?b, false)""")
-            sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
-            print(str(rule))
+            #rule.set_as_rule("""Brick(?b) -> hasBeenSet(?b, false)""")
+            #sync_reasoner_pellet()#infer_property_values=True, infer_data_property_values=True)
+            #print(str(rule))
 
-        print(self.onto.Brick.instances())
-        print(self.onto.PlaceableBrick.instances())
-        self.onto.save(file="/home/rosrunner/Desktop/repos/masterarbeit/src/ontologies/brick_deduction_filled.rdf", format="rdfxml")
+        print("all", self.onto.Brick.instances())
+        print("placeable", self.onto.PlaceableBrick.instances())
+        print("placed", self.onto.PlacedBrick.instances())
+
+        self.onto.save(file=self.working_file, format="rdfxml")
