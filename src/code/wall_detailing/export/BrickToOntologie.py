04e17d0e8b5db96e6234e6037fb018e7eee33bb9
Rossi <sebastian.rossi@elkiro.de>
2023-11-28 16:28:09 +0100

    ontology changes

--- src/code/wall_detailing/export/BrickToOntologie.py
+++ src/code/wall_detailing/export/BrickToOntologie.py
@@ -48,21 +48,27 @@ class Rule:
             property2 = ontology.holdPropertyByName[holder][0]
             print(self.effectedPropertyName, "applyObjectPropertyTo", property2)
 
+            if (ontology[self.effectedPropertyName].domain != ontology[property2].domain
+                    or ontology[self.effectedPropertyName].range != ontology[property2].range):
+                print("[WARNING] domain or range mismatch between", self.effectedPropertyName, "and", property2,
+                      ": (D", ontology[self.effectedPropertyName].domain, "vs", ontology[property2].domain,
+                      ") and (R", ontology[self.effectedPropertyName].range, "vs", ontology[property2].range, ")")
+
             for brick in ontology.NamedBrick.instances():
                 if ontology[self.effectedPropertyName] in brick.get_properties():
                     for neighbor in ontology[self.effectedPropertyName][brick]:
                         ontology[property2][brick].append(neighbor)
+
         elif ontology.CardinalityRule in self.rule.is_a:
             print("I am a cardinality rule")
         else:
-            print("I am a " + self.namespace + "." + self.name + " rule")
+            print("I am a " + self.name + " rule")
 
 
 class BrickToOntology:
     def __init__(self, bricks: List[Brick]):
         self.original_file = "file:///home/rosrunner/Desktop/repos/masterarbeit/src/ontologies/brick_deduction.rdf"
         self.working_file = "/home/rosrunner/Desktop/repos/masterarbeit/src/ontologies/temporary_working_env.rdf"
-        self.rule_file = "/home/rosrunner/Desktop/repos/masterarbeit/src/ontologies/brick_deduction.rdf"
 
         if os.name == 'nt':  # windows
             owlready2.JAVA_EXE = "C:\\Program Files\\Java\\jre-1.8\\bin\\java.exe"
@@ -70,9 +76,12 @@ class BrickToOntology:
             self.working_file = "E:\\Git\\masterarbeit\\src\\ontologies\\temporary_working_env.rdf"
 
         self.bricks = bricks
+        # self.world = World()
+
         self.onto = get_ontology(self.original_file).load()
         self.namespace = self.onto.get_namespace(self.original_file).name
 
+        # Extract rules
         rules = []
         for ruleset in self.onto.Ruleset.instances():
             rs = RuleSet(self.onto)
@@ -81,22 +90,20 @@ class BrickToOntology:
                 rs.add_rule(r)
             rules.append(rs)
 
-        # set ids
+        # Create bricks for building
+        building = self.onto.Building("building_1")
+        empty = self.onto.PlacedBrick("empty_brick")
+
         idd = 1
         dic = {}
-        self.world = World()
 
         for b in bricks:
             brick = self.onto.NamedBrick("brick_" + str(idd))
-            #brick.is_a.append(self.onto.ConcreteBrick)
             brick.hasID = [idd]
             b.id = idd
             idd += 1
             dic[b.id] = (b, brick)
 
-        building = self.onto.Building("building_1")
-        empty = self.onto.PlacedBrick("empty_brick")
-        empty.dependsOn = []
         for b in self.onto.NamedBrick.instances():
             if b == empty:
                 continue
@@ -108,41 +115,49 @@ class BrickToOntology:
             b.hasFrontNeighbor = [dic[obj.id][1] for obj in local_brick.neighbors[Neighbor.FRONT]]
             b.hasBackNeighbor = [dic[obj.id][1] for obj in local_brick.neighbors[Neighbor.BACK]]
             b.hasBeenSet = False
-            #b.dependsOn = [empty]
+            b.dependsOn = [empty]
             building.hasBrick.append(b)
 
-        #sync_reasoner_pellet(self.onto, infer_property_values=True)
-
         with self.onto:
-            def sparql():
-                sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
+            def sparql_tests():
+                sync_reasoner_hermit(infer_property_values=True) # sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
                 tmp = self.onto.world.sparql("""
-                                        PREFIX b: <http://www.semanticweb.org/rosrunner/ontologies/2023/10/brick_deduction#>
-                                        INSERT {
-                                            ?brick b:dependsOn ?n .
-                                        }
-                                        WHERE {
-                                            ?brick a b:ConcreteBrick .
-                                            ?brick b:hasBottomNeighbor ?n .
-                                        }
-                                    """)
-                print("lalala",tmp)
+                            PREFIX b: <http://www.semanticweb.org/rosrunner/ontologies/2023/10/brick_deduction#>
+                            INSERT {
+                                ?brick b:dependsOn ?n .
+                            }
+                            WHERE {
+                                ?brick a b:ConcreteBrick .
+                                ?brick b:hasBottomNeighbor ?n .
+                            }
+                            """)
+                print("lalala", tmp)
                 tmp = self.onto.world.sparql("""
-                                        PREFIX b: <http://www.semanticweb.org/rosrunner/ontologies/2023/10/brick_deduction#>
-                                        INSERT {
-                                             ?test_brick_1 rdf:type b:ConcreteBrick .
-                                            ?test_brick_1 b:hasBeenSet true .
-                                        }
-                                        WHERE {
-                                            ?test_brick_1 rdfs:subClassOf b:ConcreteBrick .
-                                        }
-                                        """)
-                #print("lalala",tmp)
-            def swrl():
+                            PREFIX b: <http://www.semanticweb.org/rosrunner/ontologies/2023/10/brick_deduction#>
+                            INSERT {
+                                 ?test_brick_1 rdf:type b:ConcreteBrick .
+                                ?test_brick_1 b:hasBeenSet true .
+                            }
+                            WHERE {
+                                ?test_brick_1 rdfs:subClassOf b:ConcreteBrick .
+                            }
+                            """)
+                tmp = list(self.onto.world.sparql("""
+                            PREFIX b: <http://www.semanticweb.org/rosrunner/ontologies/2023/10/brick_deduction#>
+                            SELECT ?brick
+                            WHERE { 
+                                ?brick a b:NamedBrick .
+                                ?brick b:hasTopNeighbor ?top .
+                            }
+                        """))
+                print("select", len(list(tmp)), list(tmp))
+            def swrl_tests():
                 pass
                 #rule = Imp(1)
                 #rule.set_as_rule("""ConcreteBrick(?b), hasBottomNeighbor(?b, ?n) -> dependsOn(?b, ?n)""")
                 #sync_reasoner_hermit(self.onto, infer_property_values=True)
+
+            sync_reasoner_hermit(infer_property_values=True)#, infer_data_property_values=True)
 
             for ruleset in rules:
                 for rule in ruleset.rules:
@@ -153,21 +168,10 @@ class BrickToOntology:
                     pass
                     close_world(b, Properties=[self.onto.dependsOn])
 
-            sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
-            one = dic[7][1]
-            print(one.get_properties())
+            sync_reasoner_hermit(infer_property_values=True) #, infer_data_property_values=True)
 
+        print("test", self.onto["hasBottomNeighbor"][self.onto.NamedBrick.instances()[8]])
 
-
-        tmp = list(self.onto.world.sparql("""
-                                            PREFIX b: <http://www.semanticweb.org/rosrunner/ontologies/2023/10/brick_deduction#>
-                                            SELECT ?brick
-                                            WHERE { 
-                                                ?brick a b:NamedBrick .
-                                                ?brick b:hasBottomNeighbor ?top .
-                                            }
-                                        """))
-        print("select", len(list(tmp)), list(tmp))
         print("all", self.onto.Brick.instances())
         print("placeable", self.onto.PlaceableBrick.instances())
         print("next", self.onto.NextBrick.instances())
