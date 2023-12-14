5ad5990ef1b7e9518fd562d0daf0e449458bc360
Sebastian Rossi <sebastian.rossi@elkiro.de>
2023-12-14 16:36:19 +0100

    WTF

--- src/code/wall_detailing/scenarios/examples_for_text/scenario2.py
+++ src/code/wall_detailing/scenarios/examples_for_text/scenario2.py
@@ -28,17 +28,28 @@ class Scenario2(Scenario):
                              quaternion.from_euler_angles(0, 0, math.pi / 2.0),
                              ifc_wall_type="LegoWallType2", name="w0")
 
-        o1 = Opening(w2, np.array([0.136 - 3 * thick_width, 0, 0]), quaternion.from_euler_angles(0, 0, 0), (4 * thin_width, 1 * thick_width, 6 * 0.096))
+        o1 = Opening(w1, np.array([0.136 - 5 * thin_width, 0, 0]), quaternion.from_euler_angles(0, 0, 0), (4 * thin_width, 1 * thick_width, 6 * 0.0096))
         o1.lintel = BrickInformation(4 * thin_width + 2 * thin_width, thick_width, 0.0096, [0.008, 0.008, 0.0096])
-        w2.openings.append(o1)
+        w1.openings.append(o1)
 
         w3 = Wall.make_wall(0.136, thick_width, height, np.array([0.192 - 0.008, 0.136 / 2.0, height / 2.0]),
                             quaternion.from_euler_angles(0, 0, math.pi / 2.0),
                             ifc_wall_type="LegoWallType2", name="w0")
 
+        o3 = Opening(w3, np.array([thin_width * 5, 0, 2 * 0.0096]), quaternion.from_euler_angles(0, 0, 0),
+                     (4 * thin_width, 1 * thick_width, 4 * 0.0096))
+        o3.lintel = BrickInformation(4 * thin_width + 2 * thin_width, thick_width, 0.0096, [0.008, 0.008, 0.0096])
+        w3.openings.append(o3)
+
         w4 = Wall.make_wall(0.064, thin_width, height, np.array([0.064 / 2 + 0.016, 0.004 + 0.072, height / 2.0]),
                             quaternion.from_euler_angles(0, 0, 0),
                             ifc_wall_type="LegoWallType1", name="w0")
+
+        o2 = Opening(w4, np.array([2 * thin_width, 0.0, 0.0]), quaternion.from_euler_angles(0, 0, 0),
+                     (4 * thin_width, 1 * thin_width, 6 * 0.0096))
+        o2.lintel = BrickInformation(4 * thin_width + 2 * thin_width, thin_width, 0.0096, [0.008, 0.008, 0.0096])
+        w4.openings.append(o2)
+
         w5 = Wall.make_wall(0.032, thin_width, height, np.array([0.088 - 0.004, 0.048 + 0.032 / 2, height / 2.0]),
                             quaternion.from_euler_angles(0, 0, math.pi / 2.0),
                             ifc_wall_type="LegoWallType1", name="w0")
